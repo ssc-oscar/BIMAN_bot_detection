0cd28221be26711330700c55fba111401346e0d3
Tapajit Dey <tapjdey@gmail.com>
2021-01-05 19:55:10 +0000

    Update match pattern

--- BIN.sh
+++ BIN.sh
@@ -1,8 +1,7 @@
-inp=$1
 while  read -r line
 do
     if `echo $line | cut -d@ -f1 | grep -qiP "([\W0-9_]bot$|^bot[\W0-9_]|[\W0-9_]bot[\W0-9_])" `
     then
         echo $line
     fi
-done < $inp
+done < "${1:-/dev/stdin}"
